802a628a8e157fc433887cd45881de960551e9ab
xiongxeng <xiongxeng@gmail.com>
2024-02-05 18:38:16 -0600

    install react-router, added starter codes for nav menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div>
+      {/*anchor link */}
     <NavBar />
-    <Home /> 
+    <a href="#home">Home</a> 
+    <Home />
+    <a href="#about">About</a> 
+    <About />
+    <a href="#projects">Projects</a> 
+    <a href="#resume">Resume</a> 
+    <a href="#Contact">Contact</a> 
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,15 +4,16 @@ import { FaBars, Fatimes} from 'react-icons/fa';
 
 const NavBar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-950 text-purple-600'>
+    <div className='fixed w-full h-[100px] flex justify-between items-center px-4 bg-slate-950 text-purple-600'>
         <div> 
-            <img src={Logo} alt = "Xeng Xiong"  style={{width: '100px'}}/> 
+            <img src={Logo} alt = "Xeng Xiong"  style={{width: '150px'}}/> 
         </div>
 
          {/* This is the nav bar menu */}
         <div>
           <ul className = 'hidden md:flex'>
             <li>Home</li>
+            <li>About</li>
             <li>Projects</li>
             <li>Resume</li>
             <li>Contact</li>
@@ -23,9 +24,10 @@ const NavBar = () => {
             <FaBars />
          </div>
 
-          {/* Moblie Menu fix later 
+          {/* Moblie Menu fix later if have time
           <ul className = 'hidden'>
             <li>Home</li>
+            <li>About</li>
             <li>Projects</li>
             <li>Resume</li>
             <li>Contact</li>
