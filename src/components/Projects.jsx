33af53d3d768774f092e38ac2669b2c8cc220bf7
xiongxeng <xiongxeng@gmail.com>
2024-02-06 20:41:59 -0600

    add codes for projects, links and images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,66 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
-    <div name = 'about' className='w-full h-screen bg-slate-950'>
-    <div className='max-w-80 mx-auto px-10 flex flex-col justify-center h-full'>
-       <p className='text-purple-600'>My Projects</p>
-       <h1 className='text-white text-4xl sm:text-5xl font-bold'>blah blah </h1>
-       <h2 className='text-purple-600 text-2xl'>blah blah blah</h2>
-     </div>
-   </div>
-  )
-}
+    <div className="w-full h-screen bg-slate-950 flex justify-center items-center">
+      <div className="max-w-80 mx-auto px-10 flex flex-col">
+        <h1 className="text-white text-4xl sm:text-5xl font-bold mb-4">
+          My Projects
+        </h1>
+        <div className="flex flex-wrap justify-center">
+          {/* Add Projects here*/}
+          <div className="project-card">
+            <a
+              href="https://github.com/Freeway9527/Xxio-WeatherApp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("../assets/images/weatherApp.png")}
+                alt="Project 1"
+                className="project-image"
+              />
+            </a>
+            <div className="project-info">
+              <p className="text-gray-300">Description of project</p>
+            </div>
+          </div>
+          <div className="project-card">
+            <a
+              href="https://github.com/your-github-repo" // Change later
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("../assets/images/weatherApp.png")} // Change later
+                alt="Project 2"
+                className="project-image"
+              />
+            </a>
+            <div className="project-info">
+              <p className="text-gray-300">Description of your project 2</p>
+            </div>
+          </div>
+          <div className="project-card">
+            <a
+              href="https://github.com/your-github-repo" // Change later
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("../assets/images/weatherApp.png")} // Change later
+                alt="Project 2"
+                className="project-image"
+              />
+            </a>
+            <div className="project-info">
+              <p className="text-gray-300">Description of your project 2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
